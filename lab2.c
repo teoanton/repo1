56aeefa39340cd990b698293209041bdffbe440f
Teodor Anton <teodoranton06@gmail.com>
2026-03-03 10:54:39 +0200

    varianta 2

--- lab2.c
+++ lab2.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int function(int x){
-    x=x-2;
-    x=function(x);
-    return x;
+int function(int x)
+{
+    return x - 2;
 }
 
-int main(){
-    int a=30, i=0;
+int main() {
+    int a = 30, i = 0;
 
-    while (a>0){
-        a=function(a);
+    while (a > 0)
+    {
+        a = function(a);
         i++;
     }
-    printf("%d", i);
+
+    printf("rez i: %d", i);
     return 0;
 }
